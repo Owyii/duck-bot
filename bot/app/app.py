287763d00ba748b13e6702dfd45826dbf16229c0
Owyii <e.ostanello@gmail.com>
2025-04-29 23:39:18 +0200

    Expanded docker configuration

--- bot/app/app.py
+++ bot/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hi from Card app test"
+    return "Hi from telegram bot"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
